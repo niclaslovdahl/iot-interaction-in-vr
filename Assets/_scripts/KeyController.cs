a59b6bcb393f0be82a0e5e14cc1e5ae091bb501c
NiclasLovdahl <niclas.lovdahl@gmail.com>
2016-11-23 21:36:17 +0100

    Added keycontroller, not yet implemented

---
+++ Assets/_scripts/KeyController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyController : MonoBehaviour {
+
+    void OnGUI() {
+        Event e = Event.current;
+
+        /* Code used to detect what key was pressed 
+		if (e.isKey) {
+			Debug.Log ("Detected key code: " + e.keyCode);
+		}
+		*/
+
+        if (e.Equals(Event.KeyboardEvent(KeyCode.Keypad1.ToString()))) {
+            
+        } else if (e.Equals(Event.KeyboardEvent(KeyCode.Keypad2.ToString()))) {
+            
+        } else if (e.Equals(Event.KeyboardEvent(KeyCode.Keypad3.ToString()))) {
+            
+        }
+
+
+    }
+
+    // Use this for initialization
+    void Start() {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}
