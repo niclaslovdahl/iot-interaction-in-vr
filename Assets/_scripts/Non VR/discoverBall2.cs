bfe064a4bb0698b55bed2d714c2def6ca2a7e702
NiclasLovdahl <niclas.lovdahl@gmail.com>
2016-12-08 16:13:37 +0100

    added offset to discoverBall2

--- Assets/_scripts/Non VR/discoverBall2.cs
+++ Assets/_scripts/Non VR/discoverBall2.cs
@@ -1,48 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class discoverBall2 : MonoBehaviour {
+public class discoverBall2 : MonoBehaviour
+{
 
-	private MeshRenderer mesh;
-	private Transform trans;
-	private bool activated;
-	private GameObject[] spheres;
+    private MeshRenderer mesh;
+    private Transform trans;
+    private bool activated;
+    private GameObject[] spheres;
 
-	public Material red;
-	public Material blue;
-	public Transform head;
-	GameObject obj;
+    public Material red;
+    public Material blue;
+    public Transform head;
+    GameObject obj;
 
-	// Use this for initialization
-	void Start() {
-		mesh = GetComponent<MeshRenderer>();
-		trans = GetComponent<Transform>();
-		mesh.material = blue;
-		spheres = GameObject.FindGameObjectsWithTag("sphere");
-		obj = GameObject.FindGameObjectWithTag ("head");
-		head = obj.GetComponent<Transform> ();
-	}
+    // Use this for initialization
+    void Start()
+    {
+        mesh = GetComponent<MeshRenderer>();
+        trans = GetComponent<Transform>();
+        mesh.material = blue;
+        spheres = GameObject.FindGameObjectsWithTag("sphere");
+        obj = GameObject.FindGameObjectWithTag("head");
+        head = obj.GetComponent<Transform>();
+    }
 
 
-	// Update is called once per frame
-	void Update() {
-		if ((trans.position.y > head.position.y) && (mesh.sharedMaterial != red)) {
-			Debug.Log("Changed color to red and summon spheres");
-			mesh.material = red;
+    // Update is called once per frame
+    void Update()
+    {
+        if ((trans.position.y > (head.position.y + 0.15f)) && (mesh.sharedMaterial != red))
+        {
+            Debug.Log("Changed color to red and summon spheres");
+            mesh.material = red;
 
-			foreach (GameObject sphere in spheres) {
-				sphere.GetComponent<MeshRenderer>().enabled = true;
-			}
+            foreach (GameObject sphere in spheres)
+            {
+                sphere.GetComponent<MeshRenderer>().enabled = true;
+            }
 
-		}
-		if ((trans.position.y <= head.position.y) && (mesh.sharedMaterial != blue)) {
-			Debug.Log("Changed color to blue and remove spheres");
-			mesh.material = blue;
+        }
+        if ((trans.position.y <= head.position.y + 0.15f) && (mesh.sharedMaterial != blue))
+        {
+            Debug.Log("Changed color to blue and remove spheres");
+            mesh.material = blue;
 
-			foreach (GameObject sphere in spheres) {
-				sphere.GetComponent<MeshRenderer>().enabled = false;
-			}
+            foreach (GameObject sphere in spheres)
+            {
+                sphere.GetComponent<MeshRenderer>().enabled = false;
+            }
 
-		}
-	}
+        }
+    }
 }
